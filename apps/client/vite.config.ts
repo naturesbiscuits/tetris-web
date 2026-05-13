08dad3c2d64637a09c84bfeced93743208f88296
NaturesBiscuits2 <flores.johnvincent.ranara@gmail.com>
2026-05-13 23:47:45 +0800

    still fixing lol

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2200,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules") && id.includes("phaser")) {
+            return "phaser";
+          }
+        }
+      }
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 5173
